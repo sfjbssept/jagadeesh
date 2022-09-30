0ff6d575956e84a536bc41d1416e863c35644ef1
unknown <jagadeesh>
2022-09-30 07:40:11 +0000

    kafka project

---
+++ kafka-project/Kafka-Publisher/src/main/java/KafkaServiceApplication.java
@@ -0,0 +1,30 @@
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class KafkaServiceApplication {
+
+	@Autowired
+	private KafkaTemplate<String, Object> template;
+	
+	private String topic = "spring-kafka";
+	
+	@GetMapping("/publish/{name}")
+	public String publishMessage(@PathVariable String name) {
+		template.send(topic,"Hi "+name+" this is inform that this message is coming from your kafka publisher");
+	return "data has been published";
+	}
+	
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaServiceApplication.class, args);
+	}
+
+}
