30a8fef1382c8dcdaaffcf15f7c5c437213ecbc0
sfjbssept <sfjbssept@gmail.com>
2022-10-11 04:29:02 +0000

    added components

--- Angular-files/starter-app/src/app/app.module.ts
+++ Angular-files/starter-app/src/app/app.module.ts
@@ -5,11 +5,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserformComponent } from './components/userform/userform.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './components/about/about.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserformComponent
+    UserformComponent,
+    NavbarComponent,
+    ContactComponent,
+    AboutComponent,
+    RegisterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
