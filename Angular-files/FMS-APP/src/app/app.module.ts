57a8b42db524fb9cabd542140ebe2ef4f3d1c2dc
sfjbssept <sfjbssept@gmail.com>
2022-10-14 10:25:37 +0000

    Flight Admin Service

--- Angular-files/FMS-APP/src/app/app.module.ts
+++ Angular-files/FMS-APP/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { FlightDetailsComponent } from './components/flight-details/flight-details.component';
+import { HomeComponent } from './components/home/home.component';
+import { AddFlightComponent } from './components/add-flight/add-flight.component';
+import { DeleteFlightComponent } from './components/delete-flight/delete-flight.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    FlightDetailsComponent,
+    HomeComponent,
+    AddFlightComponent,
+    DeleteFlightComponent
   ],
   imports: [
     BrowserModule,
