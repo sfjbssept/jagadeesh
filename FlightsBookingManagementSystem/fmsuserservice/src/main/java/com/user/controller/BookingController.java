a8068a93c1a0dfe20b2946126be29c4da11b7644
sfjbssept <sfjbssept@gmail.com>
2022-10-20 09:18:19 +0000

    Eureka server changes

--- FlightsBookingManagementSystem/fmsuserservice/src/main/java/com/user/controller/BookingController.java
+++ FlightsBookingManagementSystem/fmsuserservice/src/main/java/com/user/controller/BookingController.java
@@ -1,21 +1,57 @@
 package com.user.controller;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
 import com.user.entity.Booking;
+import com.user.entity.User;
 import com.user.service.BookingService;
+import com.user.service.UserService;
 
 public class BookingController {
 
 	@Autowired
+	RestTemplate restTemplate;
+	
+	@Autowired
+	UserService userService;
+	
+	@Autowired
 	private BookingService bookingService;
+	
+	
+	@PostMapping("/adduser")
+	public User createUser(@RequestBody User user)
+	{
+		return userService.addUser(user);
+	}
+	
+
+	@GetMapping("/getAllFlights")
+	public String getAllFlightDetails() {
+		
+		HttpHeaders headers = new HttpHeaders();
+		HttpEntity<String > httpEntity = new HttpEntity<String>(headers);
+		String url = "http://fms-admin-service/getAllFlights";
+		ResponseEntity<String> responce=restTemplate.exchange(url, HttpMethod.GET, httpEntity, String.class);
+		
+		String flightDetails = responce.getBody();
+		
+		
+		return flightDetails;
+	}
+	
 	
 	@GetMapping("/user/getBookings")
 	List<Booking> getBookings(){
