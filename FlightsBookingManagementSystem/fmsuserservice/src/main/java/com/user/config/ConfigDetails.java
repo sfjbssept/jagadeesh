2c70a948d453b785918672b84bca1abb545eb0f6
sfjbssept <sfjbssept@gmail.com>
2022-10-20 07:36:36 +0000

    Eureka server

---
+++ FlightsBookingManagementSystem/fmsuserservice/src/main/java/com/user/config/ConfigDetails.java
@@ -0,0 +1,15 @@
+package com.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+
+@Configuration
+public class ConfigDetails {
+	@Bean
+	@LoadBalanced
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+}
