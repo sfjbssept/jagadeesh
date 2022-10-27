068f2a50a88628f8217a20382702828c71c615bb
sfjbssept <sfjbssept@gmail.com>
2022-10-27 07:59:30 +0000

    flight app ui

---
+++ Angular-files/flight-apps/src/app/Entity/Flight.ts
@@ -0,0 +1,9 @@
+
+export default class Flight {
+    firstname:string= " ";
+    lastname: string = " ";
+    age:number = 0;
+    class:number = 0 ;
+    subject:string = " ";
+    gender:string= " "
+}
---
+++ Angular-files/flight-apps/src/app/components/add-airline/add-airline.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-airline',
+  templateUrl: './add-airline.component.html',
+  styleUrls: ['./add-airline.component.css']
+})
+export class AddAirlineComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
