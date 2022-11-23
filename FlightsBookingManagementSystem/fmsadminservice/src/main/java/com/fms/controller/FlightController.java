40e1d559d62f20c1e15e239aa5817d4f6c12cdd3
sfjbssept <sfjbssept@gmail.com>
2022-11-23 06:56:57 +0000

    admin and user changes

--- FlightsBookingManagementSystem/fmsadminservice/src/main/java/com/fms/controller/FlightController.java
+++ FlightsBookingManagementSystem/fmsadminservice/src/main/java/com/fms/controller/FlightController.java
@@ -3,6 +3,7 @@ package com.fms.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.fms.entity.Flight;
 import com.fms.service.IAdminFlightService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class FlightController {
 	
@@ -23,7 +25,7 @@ public class FlightController {
 		return id;
 	}
 	
-	@GetMapping("/getFlightDetails")
+	@GetMapping("/getAllFlights")
 	List<Flight> getAllFlightDetails(){
 		return flightService.getAllFlights();
 	}
--- FlightsBookingManagementSystem/fmsuserservice/src/main/java/com/user/controller/BookingController.java
+++ FlightsBookingManagementSystem/fmsuserservice/src/main/java/com/user/controller/BookingController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.user.entity.User;
 import com.user.service.BookingService;
 import com.user.service.UserService;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class BookingController {
 
